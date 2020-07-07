905cd4ace4377228416c3358150bec63740f8690
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-07 21:52:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (array,element) {
-  array.unshift(element)
-  return (element,...array)
+  array.unshift(element, ...array) 
+  return array
 }
