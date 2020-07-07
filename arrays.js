e66f4fc3dfef2843a6c9495afbb531d6f57a70f9
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-07 23:30:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   return array
 }
 
-function removeElementFromEndOfArray(array) { 
-  return array.slice(1) 
+function removeElementFromEndOfArray(array) {
+  return array.slice(1)
 }
