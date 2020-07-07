e5661c3c5a4320ad3c5feaf152adca2c21dad327
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-07 22:49:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,6 @@ function destructivelyAddElementToBeginningOfArray (array,element) {
   return array
 }
 
-function addElementToEndOfArray (array,element) { 
-  return [array,element] 
+function addElementToEndOfArray (array,element) {
+  return [array,element]
 }
